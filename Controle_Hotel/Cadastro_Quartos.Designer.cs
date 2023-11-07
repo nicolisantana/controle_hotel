dbc2037062ce1c009629d6bea50c96fe4303d002
tiagoggezf <150093336+tiagoggezf@users.noreply.github.com>
2023-11-07 12:40:07 -0300

    v.1

---
+++ Controle_Hotel/Cadastro_Quartos.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Controle_Hotel
+{
+    partial class Cadastro_Quartos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_numero = new System.Windows.Forms.TextBox();
+            this.txt_andar = new System.Windows.Forms.TextBox();
+            this.txt_tipo = new System.Windows.Forms.TextBox();
+            this.txt_descricao = new System.Windows.Forms.TextBox();
+            this.txt_preco = new System.Windows.Forms.TextBox();
+            this.lbl_numero = new System.Windows.Forms.Label();
+            this.lbl_andar = new System.Windows.Forms.Label();
+            this.lbl_tipo = new System.Windows.Forms.Label();
+            this.lbl_descição = new System.Windows.Forms.Label();
+            this.lbl_preco = new System.Windows.Forms.Label();
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.btn_alterar = new System.Windows.Forms.Button();
+            this.btn_deletar = new System.Windows.Forms.Button();
+            this.btn_verificar = new System.Windows.Forms.Button();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.btn_voltar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl_preco);
+            this.groupBox1.Controls.Add(this.lbl_descição);
+            this.groupBox1.Controls.Add(this.lbl_tipo);
+            this.groupBox1.Controls.Add(this.lbl_andar);
+            this.groupBox1.Controls.Add(this.lbl_numero);
+            this.groupBox1.Controls.Add(this.txt_preco);
+            this.groupBox1.Controls.Add(this.txt_descricao);
+            this.groupBox1.Controls.Add(this.txt_tipo);
+            this.groupBox1.Controls.Add(this.txt_andar);
+            this.groupBox1.Controls.Add(this.txt_numero);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(614, 282);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dados";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_voltar);
+            this.groupBox2.Controls.Add(this.btn_sair);
+            this.groupBox2.Controls.Add(this.btn_limpar);
+            this.groupBox2.Controls.Add(this.btn_verificar);
+            this.groupBox2.Controls.Add(this.btn_deletar);
+            this.groupBox2.Controls.Add(this.btn_alterar);
+            this.groupBox2.Controls.Add(this.btn_cadastrar);
+            this.groupBox2.Location = new System.Drawing.Point(12, 300);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(614, 206);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Opções";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(650, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(424, 494);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // txt_numero
+            // 
+            this.txt_numero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_numero.Location = new System.Drawing.Point(158, 35);
+            this.txt_numero.Name = "txt_numero";
+            this.txt_numero.Size = new System.Drawing.Size(71, 26);
+            this.txt_numero.TabIndex = 0;
+            // 
+            // txt_andar
+            // 
+            this.txt_andar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_andar.Location = new System.Drawing.Point(158, 77);
+            this.txt_andar.Name = "txt_andar";
+            this.txt_andar.Size = new System.Drawing.Size(71, 26);
+            this.txt_andar.TabIndex = 1;
+            // 
+            // txt_tipo
+            // 
+            this.txt_tipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tipo.Location = new System.Drawing.Point(158, 118);
+            this.txt_tipo.Name = "txt_tipo";
+            this.txt_tipo.Size = new System.Drawing.Size(172, 26);
+            this.txt_tipo.TabIndex = 2;
+            // 
+            // txt_descricao
+            // 
+            this.txt_descricao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_descricao.Location = new System.Drawing.Point(158, 158);
+            this.txt_descricao.Name = "txt_descricao";
+            this.txt_descricao.Size = new System.Drawing.Size(412, 26);
+            this.txt_descricao.TabIndex = 3;
+            // 
+            // txt_preco
+            // 
+            this.txt_preco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_preco.Location = new System.Drawing.Point(158, 205);
+            this.txt_preco.Name = "txt_preco";
+            this.txt_preco.Size = new System.Drawing.Size(104, 26);
+            this.txt_preco.TabIndex = 4;
+            // 
+            // lbl_numero
+            // 
+            this.lbl_numero.AutoSize = true;
+            this.lbl_numero.Location = new System.Drawing.Point(28, 43);
+            this.lbl_numero.Name = "lbl_numero";
+            this.lbl_numero.Size = new System.Drawing.Size(72, 13);
+            this.lbl_numero.TabIndex = 5;
+            this.lbl_numero.Text = "N° do Quarto:";
+            // 
+            // lbl_andar
+            // 
+            this.lbl_andar.AutoSize = true;
+            this.lbl_andar.Location = new System.Drawing.Point(28, 85);
+            this.lbl_andar.Name = "lbl_andar";
+            this.lbl_andar.Size = new System.Drawing.Size(38, 13);
+            this.lbl_andar.TabIndex = 6;
+            this.lbl_andar.Text = "Andar:";
+            // 
+            // lbl_tipo
+            // 
+            this.lbl_tipo.AutoSize = true;
+            this.lbl_tipo.Location = new System.Drawing.Point(28, 126);
+            this.lbl_tipo.Name = "lbl_tipo";
+            this.lbl_tipo.Size = new System.Drawing.Size(31, 13);
+            this.lbl_tipo.TabIndex = 7;
+            this.lbl_tipo.Text = "Tipo:";
+            // 
+            // lbl_descição
+            // 
+            this.lbl_descição.AutoSize = true;
+            this.lbl_descição.Location = new System.Drawing.Point(28, 166);
+            this.lbl_descição.Name = "lbl_descição";
+            this.lbl_descição.Size = new System.Drawing.Size(58, 13);
+            this.lbl_descição.TabIndex = 8;
+            this.lbl_descição.Text = "Descrição:";
+            // 
+            // lbl_preco
+            // 
+            this.lbl_preco.AutoSize = true;
+            this.lbl_preco.Location = new System.Drawing.Point(28, 213);
+            this.lbl_preco.Name = "lbl_preco";
+            this.lbl_preco.Size = new System.Drawing.Size(38, 13);
+            this.lbl_preco.TabIndex = 9;
+            this.lbl_preco.Text = "Preço:";
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.Location = new System.Drawing.Point(42, 36);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cadastrar.TabIndex = 0;
+            this.btn_cadastrar.Text = "Cadastrar";
+            this.btn_cadastrar.UseVisualStyleBackColor = true;
+            // 
+            // btn_alterar
+            // 
+            this.btn_alterar.Location = new System.Drawing.Point(154, 36);
+            this.btn_alterar.Name = "btn_alterar";
+            this.btn_alterar.Size = new System.Drawing.Size(75, 23);
+            this.btn_alterar.TabIndex = 1;
+            this.btn_alterar.Text = "Alterar";
+            this.btn_alterar.UseVisualStyleBackColor = true;
+            // 
+            // btn_deletar
+            // 
+            this.btn_deletar.Location = new System.Drawing.Point(278, 36);
+            this.btn_deletar.Name = "btn_deletar";
+            this.btn_deletar.Size = new System.Drawing.Size(75, 23);
+            this.btn_deletar.TabIndex = 2;
+            this.btn_deletar.Text = "Deletar";
+            this.btn_deletar.UseVisualStyleBackColor = true;
+            // 
+            // btn_verificar
+            // 
+            this.btn_verificar.Location = new System.Drawing.Point(158, 146);
+            this.btn_verificar.Name = "btn_verificar";
+            this.btn_verificar.Size = new System.Drawing.Size(75, 23);
+            this.btn_verificar.TabIndex = 3;
+            this.btn_verificar.Text = "Verificar";
+            this.btn_verificar.UseVisualStyleBackColor = true;
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Location = new System.Drawing.Point(42, 146);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(75, 23);
+            this.btn_limpar.TabIndex = 4;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.Location = new System.Drawing.Point(282, 146);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(75, 23);
+            this.btn_sair.TabIndex = 5;
+            this.btn_sair.Text = "Sair";
+            this.btn_sair.UseVisualStyleBackColor = true;
+            this.btn_sair.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btn_voltar
+            // 
+            this.btn_voltar.Location = new System.Drawing.Point(495, 92);
+            this.btn_voltar.Name = "btn_voltar";
+            this.btn_voltar.Size = new System.Drawing.Size(75, 23);
+            this.btn_voltar.TabIndex = 6;
+            this.btn_voltar.Text = "Voltar";
+            this.btn_voltar.UseVisualStyleBackColor = true;
+            // 
+            // Cadastro_Quartos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1086, 518);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Cadastro_Quartos";
+            this.Text = "Cadastro_Quartos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_preco;
+        private System.Windows.Forms.Label lbl_descição;
+        private System.Windows.Forms.Label lbl_tipo;
+        private System.Windows.Forms.Label lbl_andar;
+        private System.Windows.Forms.Label lbl_numero;
+        private System.Windows.Forms.TextBox txt_preco;
+        private System.Windows.Forms.TextBox txt_descricao;
+        private System.Windows.Forms.TextBox txt_tipo;
+        private System.Windows.Forms.TextBox txt_andar;
+        private System.Windows.Forms.TextBox txt_numero;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_voltar;
+        private System.Windows.Forms.Button btn_sair;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Button btn_verificar;
+        private System.Windows.Forms.Button btn_deletar;
+        private System.Windows.Forms.Button btn_alterar;
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Controle_Hotel/Quartos_Ocupados.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Controle_Hotel
+{
+    partial class Quartos_Ocupados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Quartos_Ocupados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 533);
+            this.Name = "Quartos_Ocupados";
+            this.Text = "Quartos_Ocupados";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
